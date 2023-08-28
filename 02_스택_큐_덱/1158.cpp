90666fb78ffae90ef334bccb700f0eb61358bfb2
OSSU <o.ss.u2056@gmail.com>
2023-08-29 00:16:21 +0900

    [스택, 큐, 덱] 8월 29일

---
+++ 02_스택_큐_덱/1158.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
+    // 입력
+    int n, k, cnt = 0;
+    cin >> n >> k;
+
+    queue<int> q;
+
+    // 사람 수 만큼 큐에 번호 추가
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
+    }
+    
+    // k의 간격이 되기 전까지 사람을 큐에서 제거하고
+    // k의 간격이 된 순간 해당 사람을 출력하며 제거
+    cout << '<';
+    int ele; // 임시 저장 변수
+    while (q.size() != 1) {
+        cnt++;
+        if(cnt % k == 0) { // 간격이 맞을 경우 출력
+            cout << q.front() << ", ";
+            q.pop();
+        } else { // 간격이 맞지 않을 경우 제거 후 가장 뒤에 추가
+            ele = q.front();
+            q.pop();
+            q.push(ele);
+        }
+    }
+
+    // 마지막 원소 출력 형태 맞추기
+    cout << q.front() << '>';
+
+    return 0;
+}
